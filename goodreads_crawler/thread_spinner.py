5a43f292b9126af6a30f8f45c113349a091545db
Michael Ershov <ershov@Michaels-MacBook-Pro-110.local>
2020-12-27 18:31:29 -0800

    Got threaded accessing of scraperAPI to work. TODO: integrate with the real code to scrape for a book. NOTE: change the click rated button to a link change to cut in half the amount of requests needed

--- goodreads_crawler/thread_spinner.py
+++ goodreads_crawler/thread_spinner.py
@@ -44,6 +44,413 @@ Author: Michael Ershov
 Date: 12/12/20
 """
 
+#-----------------------------------------------------------------------------#
+#                                  IMPORTS                                    #
+#-----------------------------------------------------------------------------#
+
+from scraper_api import ScraperAPIClient
+
+import time
+from bs4 import BeautifulSoup
+from bs4 import SoupStrainer
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import requests
+import numpy as np
+import pickle
+import sys
+import copy
+import concurrent.futures
+import threading
+import config.py as my_config
+
+#-----------------------------------------------------------------------------#
+#                            MODULE FUNCTIONS                                 #
+#-----------------------------------------------------------------------------#
+
+
+def get_page(fnc_url_link):
+    # ####### REQUEST GET METHOD for URL
+    # r = requests.get(fnc_url_link)
+    #
+    # ####### DATA FROM REQUESTS.GET
+    # data = r.text
+    # soup_toreturn = BeautifulSoup(data, 'lxml')
+
+    ##Below is the old code that WORKED
+    driver.get(fnc_url_link)
+
+    soup_toreturn = BeautifulSoup(driver.page_source, 'lxml')
+
+    time.sleep(1)
+
+    return soup_toreturn
 
 
 
+def get_page_inf_scroll(fnc_url_link, scroll_num=20):
+    ##Below is the new code for infinite scrolling
+
+    continueFlag = True
+    pageIter = 1
+    ratingsListFnc = []
+    ratingsList = None
+    ratingsScore_AVG = None
+    ratingsScore_OWN_temp = None
+
+    page_limit = 6 ##Automatically stops if more than 5 pages
+    star_limit = 3 ##Automatically stops once 3 or less stars encountered TODO: what if NONE is encountered?
+
+    while continueFlag:
+        append_string = '&page=' + str(pageIter)
+        driver.get(fnc_url_link+append_string)
+
+        temp_soup = BeautifulSoup(driver.page_source, 'lxml')
+
+        small_ratingsList = temp_soup.select('.field.title')
+        small_ratingsList.pop(0)
+        small_ratingsScore_AVG = temp_soup.select('.field.avg_rating')  # This simply gets AVERAGE RATING OF THE BOOK
+        small_ratingsScore_AVG.pop(0)
+        small_ratingsScore_OWN = temp_soup.select('.staticStars.notranslate')
+        # No need to pop from the notranslate selection
+
+        ############################
+        #####EXCTRACTION TIME#######
+        ############################
+
+        #Extract title string
+        small_ratingsList = [ currRating.contents[1].contents[1].attrs for currRating in small_ratingsList ]
+
+        #Extract the book's average rating from info string
+        small_ratingsScore_AVG = [ float(currAVG.text.split('\n')[0].split(' ')[-1]) for currAVG in small_ratingsScore_AVG ]
+
+        #Extract the actual score from the per-book rating
+        for curri, currPers in enumerate(small_ratingsScore_OWN):
+            try:
+                small_ratingsScore_OWN[curri] = star_assignment[currPers.attrs['title']]
+            except:
+                small_ratingsScore_OWN[curri] = None
+
+
+        ############################
+        ####END EXCTRACTION TIME####
+        ############################
+
+
+        if not ratingsList:
+            ratingsList = small_ratingsList.copy()
+            ratingsScore_AVG =  small_ratingsScore_AVG.copy()
+            ratingsScore_OWN = small_ratingsScore_OWN.copy()
+        else:
+            ratingsList = ratingsList + small_ratingsList.copy()
+            ratingsScore_AVG = ratingsScore_AVG + small_ratingsScore_AVG.copy()
+            ratingsScore_OWN = ratingsScore_OWN + small_ratingsScore_OWN.copy()
+
+        # To solve any bugs that arise when the ratings are None (not rated)
+        for temp_iter in range(len(small_ratingsScore_OWN)):
+            if small_ratingsScore_OWN[temp_iter] is None:
+                small_ratingsScore_OWN[temp_iter] = 0
+
+        # ratingsList_to_add = temp_soup.select('.field.title')[1:]
+        if not small_ratingsList:
+            print('We have hit the final page! final page count: {}'.format(pageIter))
+            continueFlag = False
+            #Note, this count counts the base page as 1 page...so only 1 extra page --> 2 total pages
+
+        elif pageIter >= page_limit:
+            print('We have hit the set page limit! final page count: {}'.format(pageIter))
+            continueFlag = False
+            # Note, this count counts the base page as 1 page...so only 1 extra page --> 2 total pages
+
+
+
+        elif any(temp_comp <= star_limit for temp_comp in small_ratingsScore_OWN):
+            print('We have hit the "moderately bad" rated books! final page count: {}'.format(pageIter))
+            #Remove any indices that are beyond the first 'setStar' rating
+            try:
+                first_index_beyond = ratingsScore_OWN.index(star_limit)
+            except:
+                try:
+                    first_index_beyond = ratingsScore_OWN.index(2)
+                except:
+                    try:
+                        first_index_beyond = ratingsScore_OWN.index(1)
+                    except:
+                        try:
+                            first_index_beyond = ratingsScore_OWN.index(None)
+                        except:
+                            first_index_beyond = 1 #final recourse...this should never happen
+
+
+
+            ratingsList = ratingsList[0:first_index_beyond]
+            ratingsScore_AVG = ratingsScore_AVG[0:first_index_beyond]
+            ratingsScore_OWN = ratingsScore_OWN[0:first_index_beyond]
+
+            continueFlag = False
+            # Note, this count counts the base page as 1 page...so only 1 extra page --> 2 total pages
+
+
+        else:
+            print('currently at {}'.format(len(ratingsList)))
+            pageIter += 1
+
+
+    # soup_toreturn = BeautifulSoup(driver.page_source, 'lxml')
+
+    print('soup + scroll time = {0}'.format(dT))
+
+    return ratingsList, ratingsScore_AVG, ratingsScore_OWN
+
+
+
+
+def get_payload(link_to_visit):
+    payload = {'api_key': API_KEY, 'url': link_to_visit}
+    r = requests.get('http://api.scraperapi.com', params=payload, timeout=60)
+
+    if r.status_code == 200:
+        html = r.text.strip()
+        soup = BeautifulSoup(html, 'lxml')
+    else:
+        print('STATUS CODE: {0}'.format(r.status_code))
+
+    return soup
+
+
+def get_concurrent_requests():
+    usage = client.account()
+    return usage['concurrentRequests']
+
+
+def get_next_availability(list_to_parse):
+    for i, trav_bool in enumerate(list_to_parse):
+        if not trav_bool:
+            return i
+
+
+def thread_function(thread_num):
+    print('At thread number ' + str(thread_num))
+    while not all(threading_taken): #continue while not all of threading_taken is true
+        if get_concurrent_requests() < 5:
+            with threading.Lock():
+                link_indx = get_next_availability(threading_taken)
+                threading_taken[link_indx] = True
+
+            print('Thread {0} has link {1}'.format(thread_num, link_indx))
+            get_payload(example_list[link_indx])
+
+    print('Finished with thread {0}'.format(thread_num))
+
+
+def timed_tester(which_test):
+    if which_test == 'original':
+        for i,link in enumerate(example_list):
+            get_page(link)
+            print('ORIGINAL: {0}'.format(i))
+
+
+    elif which_test == 'threading':
+        with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+            executor.map(thread_function, range(5))
+
+
+
+
+#-----------------------------------------------------------------------------#
+#                               PRE-DEFINES                                   #
+#-----------------------------------------------------------------------------#
+
+API_KEY = my_config.scraperapi_API_KEY
+client = ScraperAPIClient(API_KEY)
+
+API_KEY = '445b0c65f0d18958ea2a4cd0356bfdcb'
+
+url_location = 'https://www.goodreads.com/book/show/'
+book_reference_number = 186074 #Name of the Wind
+
+chrome_options = webdriver.chrome.options.Options()
+# chrome_options.add_argument("--headless")
+driver = webdriver.Chrome(options=chrome_options)
+
+#--------------------------------#
+#  Goodreads Star Assignment     #
+#--------------------------------#
+#  Stars   |  field rating value #
+#--------------------------------#
+#   5      |  'it was amazing'   #
+#   4      |  'really liked it'  #
+#   3      |      'liked it'     #
+#   2      |     'it was ok'     #
+#   1      |  'did not like it'  #
+#   N/A    |       No key        #
+#--------------------------------#
+
+star_assignment = {}
+
+star_assignment['it was amazing']   = 5
+star_assignment['really liked it']  = 4
+star_assignment['liked it']         = 3
+star_assignment['it was ok']        = 2
+star_assignment['did not like it']  = 1
+
+
+
+#-----------------------------------------------------------------------------#
+#                                   MAIN                                      #
+#-----------------------------------------------------------------------------#
+
+#
+# soup = get_page(url_location+str(book_reference_number))
+#
+# #Get general book info
+# book_info = {}
+#
+# book_info['title'] = soup.select('#bookTitle')[0].text.strip()  #book title
+# book_info['author'] = soup.select('.authorName')[0].text.strip() #author
+# book_info['meta'] = soup.select('#bookMeta')[0].text.strip()   #book meta
+# book_info['details'] = soup.select('#details')[0].text.strip()    #book details (stats)
+#
+# for i,v in enumerate(book_info):
+#     print('Book {0}: {1}'.format(i,v))
+#
+# #Now, step into the 30 reviewers on the first page
+# book_info['reviewers'] = list(soup.select('.user')) #Raw format (not text) of all reviewers, CONVERT TO LIST FOR EASY CONCATENATION
+#
+# #Now we need to close the popup that happens if we're not logged in...Find the close button by xpath:
+# time.sleep(.5)
+# close_XPATH = '/html/body/div[3]/div/div/div[1]/button/img'
+# element = driver.find_element_by_xpath(close_XPATH)
+# element.click()
+# time.sleep(0.95)
+#
+# #Append however many user pages we want to go through!
+# user_page_num = 4
+# for cnt_i in range(user_page_num):
+#     #instead of grabbing href link from the parsed XML, use a selenium click
+#     #won't work otherwise (since it uses a JSON request to get the next page)
+#     element = driver.find_element_by_class_name('next_page')
+#     element.click()
+#
+# curr_page_soup = BeautifulSoup(driver.page_source, 'lxml')
+# curr_page_reviews = list(curr_page_soup.select('.user'))
+# book_info['reviewers'] = book_info['reviewers'] + curr_page_reviews
+#
+# time.sleep(0.95)  # to avoid the random-length HTML garbage anti-attack mechanism
+#
+#
+#
+# reviewer_info = []
+# for i, curr_user in enumerate(book_info['reviewers']):
+#     print('Reviewer {0} ~~~~~~~~~~~~~~~~~~'.format(i))
+#     reviewer_info.append({})
+#     reviewer_info[i]['name'] = curr_user.attrs['name']
+#     reviewer_info[i]['link'] = curr_user.attrs['href']
+#
+#     #Step into the user's page
+#     goodreads_root = 'https://www.goodreads.com'
+#     t0 = time.time()
+#     user_soup = get_page(goodreads_root + reviewer_info[i]['link'])
+#     t1 = time.time()
+#     dT = t1 - t0
+#     print('user time = {0}'.format(dT))
+#
+#     #fish out the ratings link on the top left hand corner of the user's page
+#     #add it to the dictionary once we have it
+#     try:
+#         reviewer_info[i]['ratings link'] = user_soup.select('.profilePageUserStatsInfo')[0].contents[1].attrs['href']
+#     except:
+#         try:
+#             #Author's page has a different layout type...
+#             reviewer_info[i]['ratings link'] = user_soup.select('.smallText')[0].contents[1].attrs['href']
+#             print('On an authors page!')
+#         except:
+#             #Some people make their profiles private...
+#             user_text = user_soup.select('#privateProfile')[0].text
+#             usertextList = []
+#             usertextList.append( user_text.split('This')[1][1:20] )
+#             usertextList.append( user_text.split('Sign in to ')[1].split('\n')[0] )
+#
+#             print('On a private page, so no content available')
+#             print('From users page: {0}, {1}'.format(usertextList[0], usertextList[1]))
+#             print('skipping to next reviewer')
+#             continue
+#
+#
+#     #Step into the user's ratings page
+#     #NOTE: can change how the ratings are sorted by changing the "sort=ratings" bit
+#     t0 = time.time()
+#     # ratings_soup = get_page(goodreads_root + reviewer_info[i]['ratings link'])
+#     # ratings_soup = get_page_inf_scroll(goodreads_root + reviewer_info[i]['ratings link'])
+#     ratingsList, ratingsScore_AVG, ratingsScore_OWN  = get_page_inf_scroll(goodreads_root + reviewer_info[i]['ratings link'])
+#     t1 = time.time()
+#     dT = t1 - t0
+#     print('ratings time = {0}'.format(dT))
+#
+#     #iterate through all of the ratings, add it to the dictionary as a list of dictionaries (each one being the rating)
+#     #TODO: Figure out basic proxy gathering/proxy list for multithreading scraping
+#     #TODO: Once a basic 30x30 structure is saved, flesh out basics for net-creation algorithm
+#     #TODO: Get a prototype going
+#     #TODO: figure out how to make the javascript page scroll down to refesh to get all the ratings
+#
+#
+#     reviewer_info[i]['ratings'] = []
+#     for j in range(len(ratingsList)):
+#         reviewer_info[i]['ratings'].append(ratingsList[j])
+#         book_AVG_score = ratingsScore_AVG[j] #currently not used...include in the book node in the future
+#         reviewer_info[i]['ratings'][j]['score'] = ratingsScore_OWN[j]
+#
+
+
+
+
+
+#Playing with threads...
+#Just get a link with all the users
+
+example_list = ['https://www.goodreads.com/user/show/2465351-ian',
+                'https://www.goodreads.com/user/show/3030788-melanie']
+
+example_list = ['https://www.goodreads.com/user/show/2465351-ian',
+                'https://www.goodreads.com/user/show/59458347-petrik',
+                'https://www.goodreads.com/user/show/395599-shannon-giraffe-days',
+                'https://www.goodreads.com/user/show/666037-danica',
+                'https://www.goodreads.com/author/show/4721536.Mark_Lawrence',
+                'https://www.goodreads.com/user/show/3639270-rob',
+                'https://www.goodreads.com/user/show/58160628-emily-books-with-emily-fox',
+                'https://www.goodreads.com/author/show/108424.Patrick_Rothfuss',
+                'https://www.goodreads.com/user/show/17438949-melissa-dog-wolf-lover-martin',
+                'https://www.goodreads.com/user/show/10171516-jessica',
+                'https://www.goodreads.com/user/show/69106439-virginia-ronan-herondale',
+                'https://www.goodreads.com/user/show/58940-debbie',
+                'https://www.goodreads.com/user/show/3030788-melanie',
+                'https://www.goodreads.com/user/show/2465351-ian',
+                'https://www.goodreads.com/user/show/59458347-petrik',
+                'https://www.goodreads.com/user/show/395599-shannon-giraffe-days',
+                'https://www.goodreads.com/user/show/666037-danica',
+                'https://www.goodreads.com/author/show/4721536.Mark_Lawrence',
+                'https://www.goodreads.com/user/show/3639270-rob',
+                'https://www.goodreads.com/user/show/58160628-emily-books-with-emily-fox',
+                'https://www.goodreads.com/author/show/108424.Patrick_Rothfuss',
+                'https://www.goodreads.com/user/show/17438949-melissa-dog-wolf-lover-martin',
+                'https://www.goodreads.com/user/show/10171516-jessica',
+                'https://www.goodreads.com/user/show/69106439-virginia-ronan-herondale',
+                'https://www.goodreads.com/user/show/58940-debbie',
+                'https://www.goodreads.com/user/show/3030788-melanie']
+
+threading_taken = [False]*len(example_list) #create a mapping list that tracks which of the lists are taken
+
+
+
+
+#Run the actual tests!
+
+currtime = time.time()
+timed_tester('original')
+fintime = time.time()
+print('Original time: {0}'.format(fintime - currtime))
+
+currtime = time.time()
+timed_tester('threading')
+fintime = time.time()
+print('Threading time: {0}'.format(fintime - currtime))
